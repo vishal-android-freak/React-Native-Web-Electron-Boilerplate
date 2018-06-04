16cdc8e117f747afb6e1696c9a8208877c0d421a
Vishal Dubey <yzvishal.vd@gmail.com>
2018-06-04 14:24:04 +0530

    initial code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { View, Text } from 'react-native-web';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hi</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
